dfdbb78d122ce723eecaea1fbc10625d18805643
divagar1234 <divapandu63@gmail.com>
2026-03-09 11:44:08 +0530

    Add files via upload

---
+++ FrameDemo.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+class FrameDemo {
+    public static void main(String arg[]) {
+        Frame f = new Frame();
+        f.setTitle("Divagar-BCA");
+        f.setSize(500, 400);
+        f.setVisible(true);
+    }
+}
